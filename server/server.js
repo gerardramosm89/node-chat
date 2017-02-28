daca946eea4e4054c057ee6de50c9f4f598787ab
gramos <gerard.ramosm89@gmail.com>
2017-02-27 20:59:36 -0800

    chatting from one window works

--- server/server.js
+++ server/server.js
@@ -1,46 +1,30 @@
-const path       = require('path');
-const http       = require('http');
-const express    = require('express');
-const socketIO   = require('socket.io');
-var app          = express();
-const publicPath = path.join(__dirname, '../public');
-const port       = process.env.PORT || 3000;
-var server       = http.createServer(app);
-var io           = socketIO(server);
+const path                = require('path');
+const http                = require('http');
+const express             = require('express');
+const socketIO            = require('socket.io');
+const { generateMessage } = require('./utils/message');
+var app                   = express();
+const publicPath          = path.join(__dirname, '../public');
+const port                = process.env.PORT || 3000;
+var server                = http.createServer(app);
+var io                    = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-    socket.emit('newMessage', {
-      from: 'Admin',
-      text: 'Welcome to the chat App',
-      createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', {
-      from: 'Admin',
-      text: 'New user has joined',
-      createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
-  socket.on('createMessage', (message) => {
-    //console.log(`Create message is: ${JSON.stringify(message)}`);
-    // io.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
-
-    socket.broadcast.emit('newMessage', {
-      from: message.from,
-      to: message.to,
-      createdAt: new Date().getTime()
-    })
-  });
-  socket.on('disconnect', () => {
-    console.log('user was disconnected');
-  });
+    socket.on('createMessage', (message, callback) => {
+      console.log('createMessage', message);
+      io.emit('newMessage', generateMessage(message.from, message.text))
+      callback('This is from the server');  
+    });
+    socket.on('disconnect', () => {
+      console.log('user was disconnected');
+    });
 });
 server.listen(port, () => {
   console.log(`App is listening on Port ${port}`);
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,17 +1,11 @@
 var expect = require('expect');
-var generateMessage = require('./message');
-// var generateMessage = (from, text) => {
-//   return {
-//     from,
-//     text,
-//     createdAt: new Date().getTime()
-//   };
-// };
+var {generateMessage} = require('./message');
+
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var from = 'Jen';
     var text = 'Some message';
-    var message = generateMessage.generateMessage(from, text);
+    var message = generateMessage(from, text);
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
