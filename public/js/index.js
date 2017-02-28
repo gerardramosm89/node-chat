df0fe993b0927b321ab03c5cdaa91740ae7a896a
gramosubuntu <gerard.ramosm89@gmail.com>
2017-02-28 12:48:27 -0800

    added momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,10 @@
   });
 
   socket.on('newMessage', function (message){
-    console.log('New Message', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    console.log('New time is: ', formattedTime);
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
   });
@@ -19,8 +20,8 @@
 socket.on('newLocationMessage', function(message){
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from}: `);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
