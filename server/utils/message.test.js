1feb0d064a79690cd1429e4c377eb3fbeaabf7e7
gramosubuntu <gerard.ramosm89@gmail.com>
2017-02-28 10:41:11 -0800

    added test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,7 @@
-var expect = require('expect');
-var {generateMessage} = require('./message');
+var expect                      = require('expect');
+var { generateMessage }         = require('./message');
+var { generateLocationMessage } = require('./message');
+
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,5 +11,19 @@ describe('generateMessage', () => {
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+
+describe('generateLocationMessage', () => {
+  it('should generate location message', () => {
+    var from = 'Gerry';
+    var latitude = 15;
+    var longitude = 19;
+    var url='https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
