2d9c703fd9ac8322017a560c16244b78a7dcb0f8
gramos <gerard.ramosm89@gmail.com>
2017-02-28 21:25:05 -0800

    added tests for users

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,30 +1,42 @@
 
 
 class Users {
-  constructor(){
+  constructor() {
     this.users = [];
   }
 
-  addUser(){
-    
+  addUser(id, name, room){
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+
+  removeUser(id) {
+    var user = this.getUser(id);
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+
+  getUser(id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+
+  getUserList(room){
+    var users = this.users.filter((user) => {
+      return user.room === room;
+    });
+
+    var namesArray = users.map((user) => {
+      return user.name;
+    });
+
+    return namesArray;
   }
 }
-// addUser(id, name, room)
-// removeUser(id)
-// getUser(id)
-// getUserList(room)
 
-// class Person {
-//   constructor(name, age){
-//     this.name = name;
-//     this.age = age;
-//   }
-
-//   getUserDescription () {
-//     return `${this.name} is ${this.age}`;
-//   }
-// }
-
-// var me = new Person("Gerry", 21);
-// console.log(me);
-// console.log(me.getUserDescription());
+module.exports = {
+  Users
+}
