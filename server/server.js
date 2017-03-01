8dda5c71045023b8c145c7c734c3fab0bab370e1
gramosubuntu <gerard.ramosm89@gmail.com>
2017-02-28 16:54:38 -0800

    starting users

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express             = require('express');
 const socketIO            = require('socket.io');
 const { generateMessage, 
   generateLocationMessage } = require('./utils/message');
+const { isRealString }    = require('./utils/validation');
 var app                   = express();
 const publicPath          = path.join(__dirname, '../public');
 const port                = process.env.PORT || 3000;
@@ -13,11 +14,24 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log("New user connected");
-
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
+    socket.on('join', (params, callback) => {
+      if (!isRealString(params.name) || !isRealString(params.room)) {
+        callback('Name and room name are required');
+      }
+
+      socket.join(params.room);
+
+      //io.emit emits to everyone on the server
+      //socket.broadcast.emit sends a message to everyone connected to the server
+      //socket.emit sends directly to one user
+      //socket.leave("room")
+
+      socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+      socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin',  `${params.name} has joined.`));
+      callback();
+    });
     socket.on('createMessage', (message, callback) => {
       console.log('createMessage', message);
       io.emit('newMessage', generateMessage(message.from, message.text))
@@ -25,7 +39,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-      io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+      io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
---
+++ server/utils/users.js
@@ -0,0 +1,30 @@
+
+
+class Users {
+  constructor(){
+    this.users = [];
+  }
+
+  addUser(){
+    
+  }
+}
+// addUser(id, name, room)
+// removeUser(id)
+// getUser(id)
+// getUserList(room)
+
+// class Person {
+//   constructor(name, age){
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   getUserDescription () {
+//     return `${this.name} is ${this.age}`;
+//   }
+// }
+
+// var me = new Person("Gerry", 21);
+// console.log(me);
+// console.log(me.getUserDescription());
