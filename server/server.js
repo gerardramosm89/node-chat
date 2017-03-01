42dac50b3db7557bfa924ad89221155dd26b4450
gramosubuntu <gerard.ramosm89@gmail.com>
2017-03-01 07:45:51 -0800

    sends only to users in room

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-const path                = require('path');
-const http                = require('http');
-const express             = require('express');
-const socketIO            = require('socket.io');
+const path                  = require('path');
+const http                  = require('http');
+const express               = require('express');
+const socketIO              = require('socket.io');
 const { generateMessage, 
   generateLocationMessage } = require('./utils/message');
-const { isRealString }    = require('./utils/validation');
-var app                   = express();
-const publicPath          = path.join(__dirname, '../public');
-const port                = process.env.PORT || 3000;
-var server                = http.createServer(app);
-var io                    = socketIO(server);
-const { Users } = require('./utils/users');
-var users = new Users();
+const { isRealString }      = require('./utils/validation');
+var app                     = express();
+const publicPath            = path.join(__dirname, '../public');
+const port                  = process.env.PORT || 3000;
+var server                  = http.createServer(app);
+var io                      = socketIO(server);
+const { Users }             = require('./utils/users');
+var users                   = new Users();
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
@@ -38,13 +38,17 @@ io.on('connection', (socket) => {
       callback();
     });
     socket.on('createMessage', (message, callback) => {
-      console.log('createMessage', message);
-      io.emit('newMessage', generateMessage(message.from, message.text))
+      //console.log('createMessage', message);
+      var user = users.getUser(socket.id);
+      if (user && isRealString(message.text)){
+        io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+      }
       callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
-      io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+      var user = users.getUser(socket.id);
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
